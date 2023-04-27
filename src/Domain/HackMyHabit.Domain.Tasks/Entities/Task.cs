ad5177370b1719472f7c26b3ba786aa0384c5c82
blahutakrystian <blahutakrystian@gmail.com>
2023-04-27 18:21:57 +0200

    feat(model): add TaskList as task aggregate root

--- src/Domain/HackMyHabit.Domain.Tasks/Entities/Task.cs
+++ src/Domain/HackMyHabit.Domain.Tasks/Entities/Task.cs
@@ -1,25 +1,25 @@
-﻿namespace HackMyHabit.Domain.Tasks.Entities
+﻿using HackMyHabit.Domain.Commons.Abstractions;
+
+namespace HackMyHabit.Domain.Tasks.Entities
 {
-    public class Task
+    public class Task: BaseEntity
     {
-        public Guid Id { get; private set; }
         public string Name { get; private set; }
         public string Description { get; private set; }
         public DateTime DueDate { get; private set; }
         public DateTime CreationDate { get; private set; }
         public bool IsCompleted { get; private set; }
-        //TODO: to replace by User class as creator
-        public Guid UserId { get; private set; }
+        public Guid TaskListId { get; private set; }
 
-        public Task(Guid id, string name, string description, DateTime dueDate, Guid userId, DateTime creationDate)
+        public Task(Guid id, string name, string description, DateTime dueDate, Guid userId, DateTime creationDate, Guid taskListId)
         {
             Id = id;
             Name = name;
             Description = description;
             DueDate = dueDate;
             IsCompleted = false;
-            UserId = userId;
             CreationDate = creationDate;
+            TaskListId = taskListId;
         }
 
         public void MarkAsCompleted()
---
+++ src/Domain/HackMyHabit.Domain.Tasks/Exceptions/InvalidTaskNameAlreadyExists.cs
@@ -0,0 +1,17 @@
+﻿using HackMyHabit.Domain.Commons.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackMyHabit.Domain.Tasks.Exceptions
+{
+    internal class InvalidTaskNameAlreadyExists : CustomException
+    {
+        public InvalidTaskNameAlreadyExists(string taskName) : 
+            base($"The task with the given name({taskName}) already exists")
+        {
+        }
+    }
+}
